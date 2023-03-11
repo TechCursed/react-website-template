fd23790c699ee2a3f59915f5348273a991d2762c
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-03-11 23:47:33 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Home.css'
 import backgroundHome from '../images/figuretext.png';
-// import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
@@ -14,9 +13,9 @@ function Home() {
 
       <div class="col-sm-6">
         <div className='left'>
-        <h1>Welcome to this react website, we really appriciate your time </h1>
+        <h1>Welcome to my react website, I really appriciate your time </h1>
         <h3>Let's build great stuff together.</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> */}
         </div>
 
         <div className='home-button'>
@@ -35,25 +34,6 @@ function Home() {
 
     </div>
   </div>
-   
-    // <div class="container-box">
-    
-    // <div className='home-content'>
-    // <h1>This is h1</h1>
-    // <h2>This is h2</h2>
-    // <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    // </div>
-
-    // <div className="home-img"> 
-    // <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-    // </div>
-
-
-
-    // </div>
-
-    
    
   )
 }
