17cc157b5ebae79375c377d8e9f70954ddc40bf0
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-03-12 15:48:08 +0530

    update css

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ function About() {
     <div className='left'>
     <h1>React is beautiful</h1>
     <p>I've always developed stuff in vanilla javascript, and the moment I got familiar with react, it was  absolutely a game changer for me, it's a vast library and I'm getting there slowly, one step at a time.</p>
-    <h2>This website use the concepts like</h2>
+    <h2>This website use the following tools</h2>
 
     <div className='details'>
     <h4>React-Router-DOM</h4>
